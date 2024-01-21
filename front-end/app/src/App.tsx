47a67326f5439735f4340e94ae2ffa2fb5f91657
Kenttrann2302 <110959350+Kenttrann2302@users.noreply.github.com>
2024-01-21 02:46:45 -0500

    finish create modal

--- front-end/app/src/App.tsx
+++ front-end/app/src/App.tsx
@@ -1,15 +1,18 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import LoadingContextProvider from "./context/LoadingContext";
 import Home from "./views/Home/Home";
+import AppContextProvider from "./context/AppContext";
 
 const App = () => {
   return (
     <Router>
-      <LoadingContextProvider>
-        <Routes>
-          <Route path="/home" element={<Home />} />
-        </Routes>
-      </LoadingContextProvider>
+      <AppContextProvider>
+        <LoadingContextProvider>
+          <Routes>
+            <Route path="/home" element={<Home />} />
+          </Routes>
+        </LoadingContextProvider>
+      </AppContextProvider>
     </Router>
   );
 };
