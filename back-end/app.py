6f82c90d681f2770736fd0898859aecef81f0519
Dinhbaon <107369617+Dinhbaon@users.noreply.github.com>
2024-01-21 01:40:16 -0500

    backend refactor

--- back-end/app.py
+++ back-end/app.py
@@ -1,17 +1,15 @@
-from flask import request, jsonify, Flask
-from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime, timedelta
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
+import datetime
 import uuid
-from sqlalchemy import select
+from flask import Flask, request
+from models import db
+from flask_sqlalchemy import SQLAlchemy
+import pandas as pd
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy()
 app.config['SQLALCHEMY_DATABASE_URI'] = 'cockroachdb://dat:a8yS_VjWUTTMdhfXLQ70Cw@waning-reptile-6926.g8z.gcp-us-east1.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full'
-db = SQLAlchemy(app)
-Base = declarative_base()
+db.init_app(app)
 
 def getFixTime(carType : str):
     if (carType == 'compact'):
@@ -31,59 +29,20 @@ class AppointmentObject:
         self.id = id 
         self.dateBooked = dateBooked
         self.dateStartAppointment = dateStartAppointment
-        self.dateEndAppointment = datetime.strptime(dateStartAppointment, '%m/%d/%Y %I:%M:%S %p') + timedelta(minutes=getFixTime(carType))
+        self.dateEndAppointment = datetime.strptime(dateStartAppointment, '%m/%d/%Y %I:%M:%S %p') + datetime.timedelta(minutes=getFixTime(carType))
         self.carType = carType
-
-
-
-class BaseTable(db.Model):
-    __abstract__ = True
-
-    id = db.Column(db.String(100), primary_key=True)
-    dateBooked = db.Column(db.String(40))
-    dateStartAppointment = db.Column(db.String(40))
-    dateEndAppointment = db.Column(db.String(40))
-    carType = db.Column(db.String(40))
-
-# Create 10 tables with names BayTable1, BayTable2, ..., BayTable10
-for i in range(1, 11):
-    table_name = f'BayTable{i}'
-
-    # Define a class dynamically with the desired table name and inherit from BaseTable
-    class_name = f'BayTable{i}'
-    table_class = type(class_name, (BaseTable,), {'__tablename__': table_name})
-    
-    # Add the class to the global namespace (optional)
-    globals()[class_name] = table_class
-    
-
-
-# Assuming the tables were dynamically created with names BayTable1, BayTable2, ..., BayTable10
-table_names = [f'BayTable{i}' for i in range(1, 11)]
-
-# Create a list to store Table objects for each table name
-tables = []
-
-# Query each table and print the results
 with app.app_context():
-    for i in range(1, 11):
-        table_name = f'BayTable{i}'
-        table_class = globals().get(table_name)
-
-        if table_class:
-            query = table_class.query.all()
-            print(f"Results for {table_name}:")
-            for row in query:
-                print(row.__dict__)
-            print("\n")
+    db.create_all()
 
 
 @app.route('/')
 def hello():
     return '<h1>Hello, World!</h1>'
 
-@app.route('/predict', methods=['POST'])
+@app.route('/upload', methods=['POST'])
 def predict():
+    file = request.files["file"]
+    print(pd.read_csv(file))
     loAppointmentObject : list[AppointmentObject] = request.form.get('table')
     for appointment in loAppointmentObject:
         appointmentObject = AppointmentObject(  str(uuid.uuid1()), 
@@ -104,3 +63,4 @@ lambda_object = {
     'class-1':1982/(24*60), 
 
 }
+  # Create tables when the app starts
--- back-end/prediction-algo.py
+++ back-end/prediction-algo.py
@@ -1,3 +1,19 @@
-from app import AppointMentObject
+from app import db, app 
 
-def predictionAlgo(baySetUp, request : AppointMentObject):
+# def predictionAlgo(baySetUp, request : AppointMentObject):
+
+def query_table(table_name):
+    with app.app_context():
+        table_class = globals().get(table_name)
+        print(table_class)
+        if table_class:
+            # Query the table
+            results = table_class.query.all()
+
+            # Process the results as needed
+            print(f"Results for {table_name}:")
+            for row in results:
+                print(row.__dict__)
+            print("\n")
+
+query_table('BayTable1')
