f6e0733c3f08e060037b854235295428f776ebed
Kenttrann2302 <110959350+Kenttrann2302@users.noreply.github.com>
2024-01-21 10:08:20 -0500

    finish line-chart

--- front-end/app/src/types/date.ts
+++ front-end/app/src/types/date.ts
@@ -1,7 +1,5 @@
-import { DatePickerData } from "../components/DatePicker/DatePicker";
-
 // date types
 export type PostDateReq = {
     url: string;
-    date: DatePickerData;
+    date: string;
 };
