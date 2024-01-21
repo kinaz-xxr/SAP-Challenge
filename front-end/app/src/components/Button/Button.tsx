9e53fcb011824869c92afe5781aeaed6985dc993
Kenttrann2302 <110959350+Kenttrann2302@users.noreply.github.com>
2024-01-21 02:55:12 -0500

    finish button component

--- front-end/app/src/components/Button/Button.tsx
+++ front-end/app/src/components/Button/Button.tsx
@@ -1,19 +1,18 @@
 // button component
-import styles from './Button.module.scss';
+import styles from "./Button.module.scss";
 
 export type ButtonProps = {
-    label?: string;
-    onClick: (event: any) => void;
+  label?: string;
+  onClick: (event: any) => void;
+  className?: string;
 };
 
-const Button = (
-    props: ButtonProps
-) => {
-    return (
-        <button className={styles.Button} onClick={props.onClick}>
-            {props.label}
-        </button>
-    )
+const Button = (props: ButtonProps) => {
+  return (
+    <button className={props.className ? props.className : styles.Button}>
+      {props.label}
+    </button>
+  );
 };
 
 export default Button;
