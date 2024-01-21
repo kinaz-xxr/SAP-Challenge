7af333355983cf0f6d375cb7306ab0d89f49b235
Kenttrann2302 <110959350+Kenttrann2302@users.noreply.github.com>
2024-01-21 03:31:25 -0500

    finish success component

--- front-end/app/src/App.tsx
+++ front-end/app/src/App.tsx
@@ -2,15 +2,18 @@ import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import LoadingContextProvider from "./context/LoadingContext";
 import Home from "./views/Home/Home";
 import AppContextProvider from "./context/AppContext";
+import SuccessContextProvider from "./context/SuccessContext";
 
 const App = () => {
   return (
     <Router>
       <AppContextProvider>
         <LoadingContextProvider>
-          <Routes>
-            <Route path="/home" element={<Home />} />
-          </Routes>
+          <SuccessContextProvider>
+            <Routes>
+              <Route path="/home" element={<Home />} />
+            </Routes>
+          </SuccessContextProvider>
         </LoadingContextProvider>
       </AppContextProvider>
     </Router>
